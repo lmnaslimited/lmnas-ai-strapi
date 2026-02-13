a82e04fcdffd616bcccd89e27f13c6788b5511a4
Arunkumar Ganesan <67154864+arun-lmnas@users.noreply.github.com>
2026-02-13 20:08:09 +0530

    Create benefit-question collection type

---
+++ src/api/benefit-question/controllers/benefit-question.ts
@@ -0,0 +1,7 @@
+/**
+ * benefit-question controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::benefit-question.benefit-question');
---
+++ src/api/benefit-question/routes/benefit-question.ts
@@ -0,0 +1,7 @@
+/**
+ * benefit-question router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::benefit-question.benefit-question');
